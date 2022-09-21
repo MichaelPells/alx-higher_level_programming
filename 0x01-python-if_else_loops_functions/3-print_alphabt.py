0d592bf6bb5a8fa37797077064980a8080fc6b8b
Michael Pells <themichaelpells@gmail.com>
2022-09-21 03:17:03 +0100

    3-print_alphabt.py*

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 
 i = 97
 while i <= 122:
-    print("{}".format(chr(i) if chr(i) in ['q', 'e'] else ''), end="")
+    print("{}".format(chr(i) if chr(i) not in ['q', 'e'] else ''), end="")
     i += 1
