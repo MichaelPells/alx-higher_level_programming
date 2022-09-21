a78f9ba26e903d8648eb8f23accb58e44914766c
Michael Pells <themichaelpells@gmail.com>
2022-09-21 03:27:50 +0100

    5-print_comb2.py*

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    print("{:02d}".format(i),
-	end=f"""{', ' if i < 99 else '''
-'''}""")
+    print("{}{:02d}".format(', ' if i > 0 else '', i), end="")
+
+print()
