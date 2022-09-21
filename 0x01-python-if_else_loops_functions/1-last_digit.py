d882ea142222bf5c793321df3b9ba6a5c671b4d3
Michael Pells <themichaelpells@gmail.com>
2022-09-21 01:19:13 +0100

    2-print_alphabet.py*

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last = (abs(number) % 10) * int(number / abs(number))
+
+if last > 5:
+	report = "and is greater than 5"
+elif last == 0:
+	report = "and is 0"
+else:
+	report = "and is less than 6 and not 0"
+
+print(f"Last digit of {number} is {last} {report}")
